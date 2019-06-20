cce3cb92637105f3ceebef55c9859e0dbfc2db88
Neil Peterson <neilpeterson@outlook.com>
2019-06-20 16:17:36 -0700

    Content Updates

--- deployment-artifacts/deployment/deploy.sh
+++ deployment-artifacts/deployment/deploy.sh
@@ -1,4 +1,3 @@
 echo "Vault Name: $VAULT_NAME"
 echo "Secret Name: $SECRET_NAME"
-
-echo $(az keyvault secret show --name $SECRET_NAME --vault-name $VAULT_NAME --query value -o tsv)
+echo "Secret Value: $(az keyvault secret show --name $SECRET_NAME --vault-name $VAULT_NAME --query value -o tsv)"
